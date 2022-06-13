0833a01ff4056fef4e6126005f396dcbf38b07c6
xuehao <larissa_x@163.com>
2022-06-13 15:01:53 +0800

    【demand】<新增sql>

--- lib_core/src/main/java/com/qlcd/android/ui/database/dao/LoggerDao.kt
+++ lib_core/src/main/java/com/qlcd/android/ui/database/dao/LoggerDao.kt
@@ -33,34 +33,9 @@ interface LoggerDao {
     @Query(value = "SELECT DISTINCT level FROM logger_table")
     suspend fun queryAllLevel(): List<String>
 
-    /**
-     * 时间降序查询
-     * 按条件查询logger_table数据
-     * 全部必传，sql需要优化
-     */
-    @Query(value = "SELECT * FROM logger_table WHERE level= :level AND fileName= :fileName AND time>=:time ORDER BY time DESC LIMIT (:page-1)* :pageNum,:pageNum")
-    suspend fun queryAllLoggersByDesc(
-        level: String,
-        fileName: String,
-        time: Long,
-        page: Int,
-        pageNum: Int = 10,
-    ): List<LoggerEntity>
-
-    /**
-     * 时间升序查询
-     */
-    @Query(value = "SELECT * FROM logger_table WHERE level= :level AND fileName= :fileName AND time>=:time ORDER BY time ASC LIMIT (:page-1)* :pageNum,:pageNum")
-    suspend fun queryAllLoggersByAsc(
-        level: String? = "*",
-        fileName: String? = "*",
-        time: Long? = 0,
-        page: Int = 1,
-        pageNum: Int = 10,
-    ): List<LoggerEntity>
 
     @RawQuery
-    suspend fun queryAllLoggersByDesc(query: SupportSQLiteQuery): List<LoggerEntity>
+    suspend fun customQueryAllLoggers(query: SupportSQLiteQuery): List<LoggerEntity>
 
 
     /**
@@ -86,6 +61,9 @@ interface LoggerDao {
                 val calendar = Calendar.getInstance()
                 calendar.time = string2Date
                 calendar.set(Calendar.HOUR_OF_DAY, 0)
+                calendar.set(Calendar.MINUTE, 0);
+                calendar.set(Calendar.SECOND, 0);
+                calendar.set(Calendar.MILLISECOND, 0);
                 //当天0点
                 val startTime = calendar.time.time
                 calendar.set(Calendar.HOUR_OF_DAY, 23);
@@ -118,6 +96,6 @@ interface LoggerDao {
         buffer.append("LIMIT ${(page-1)*pageNum},${pageNum}")
 
         LogUtils.d(buffer.toString())
-        return queryAllLoggersByDesc(SimpleSQLiteQuery(buffer.toString()))
+        return customQueryAllLoggers(SimpleSQLiteQuery(buffer.toString()))
     }
 }
