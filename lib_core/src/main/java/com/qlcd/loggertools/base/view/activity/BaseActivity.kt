ed439b8fd0f5a3a5d12e1cf74c42a50f2ebceadf
gaoluhan <gaoluhan@smzdm.com>
2022-06-14 18:55:57 +0800

    剔除无用依赖

--- lib_core/src/main/java/com/qlcd/loggertools/base/view/activity/BaseActivity.kt
+++ lib_core/src/main/java/com/qlcd/loggertools/base/view/activity/BaseActivity.kt
@@ -1,22 +1,17 @@
 package com.qlcd.loggertools.base.view.activity
 
 import android.os.Bundle
-import android.view.View
 import androidx.annotation.CallSuper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import com.blankj.utilcode.util.ColorUtils
-import com.qlcd.loggertools.R
 import com.qlcd.loggertools.base.view.IView
-import com.qlcd.loggertools.base.view.loading.ZLoadingDialog
 import com.qlcd.loggertools.base.viewmodel.BaseViewModel
 import com.qlcd.loggertools.logger.LogKit
 
 
 abstract class BaseActivity :
     AppCompatActivity(), IView {
-    private var dialog: ZLoadingDialog? = null
 
     @CallSuper
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +25,7 @@ abstract class BaseActivity :
     }
 
     protected inline fun <reified T : ViewDataBinding> binding(): Lazy<T> = lazy {
-        val binding : T =  DataBindingUtil.setContentView(this,bindLayout())
+        val binding: T = DataBindingUtil.setContentView(this, bindLayout())
         binding.lifecycleOwner = this
         return@lazy binding
     }
@@ -42,14 +37,6 @@ abstract class BaseActivity :
             }
         }
     }
-
-    /**
-     * 绑定展示空页面时替换的布局
-     */
-    open fun bindShowEmptyView(): View? {
-        return null
-    }
-
 
     /**
      * 绑定布局
@@ -74,30 +61,9 @@ abstract class BaseActivity :
      */
     open fun initWindow() {}
 
-    /**
-     * Loading
-     * 不可直接调用
-     */
-    private fun showLoading() {
-        dialog?.apply {
-            if (isShowing) {
-                return@showLoading
-            }
-        }
-
-        if (dialog == null) {
-            dialog = ZLoadingDialog(this)
-                .setLoadingColor(ColorUtils.getColor(R.color.c_AppColor_01))
-//                .setHintText(AppUtils.getAppName())
-//                .setHintTextColor(Color.GRAY)
-        }
-        dialog?.show()
-    }
-
 
     protected open fun setStatusBar() {
 
     }
-
 
 }
